4a14147411f7d4dd6bf12838037eadc7ed2171c3
M. Różycki <kudlatek47@gmail.com>
2019-10-15 23:33:00 +0200

    Geberate Spring Boot Application
    Application generated by https://start.spring.io/
    
    Used:
    - DevTools
    - Lombok
    - Spring Web

---
+++ src/main/java/pl/com/rozyccy/dinnersMenu/DinnersMenuApplication.java
@@ -0,0 +1,13 @@
+package pl.com.rozyccy.dinnersMenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinnersMenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinnersMenuApplication.class, args);
+	}
+
+}
